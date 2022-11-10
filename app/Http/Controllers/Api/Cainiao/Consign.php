d840c8ffaf9de80bebdf49ea09eac597c4b4812b
Eva <1046002493@qq.com>
2022-11-10 17:03:21 +0800

    '处理菜鸟部分APIV2'

--- app/Http/Controllers/Api/Cainiao/Consign.php
+++ app/Http/Controllers/Api/Cainiao/Consign.php
@@ -31,26 +31,11 @@ class Consign extends Controller
 
 
         $param = $request->all();
-//
-//        if(!$request->filled(['logistics_interface', 'partner_code', 'from_code', 'msg_type'])){
-//
-//            return response()->json(["success" => false, "message" => "预报接收失败", "code" => 400413]);
-//        }
-//
-//        if($param['partner_code'] != 'TRAN_STORE_30792933' || $param['from_code'] != 'CNCTP' || $param['msg_type'] != 'CONSO_WAREHOUSE_CONSIGN'){
-//
-//
-//            return response()->json(["success" => false, "message" => "预报接收失败", "code" => 400416]);
-//        }
-
-        //处理接收的预报件--彩虹桥部分
 
 
-        Caoniaotext::create(['text'=>json_encode($param)]);
+        Caoniaotext::create(['text' => json_encode($param)]);
 
-
-        return $this->ReturnJson(200201,'ok',[],200);
+        return $this->ReturnCainiao('下发成功');
     }
-
 
 }
